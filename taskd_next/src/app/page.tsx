871625789148c4cba8193565f96d38b31cf08826
aaron-nerox <haroune.official2@gmail.com>
2024-08-04 20:50:06 +0400

    add: new favicon for the website

--- taskd_next/src/app/page.tsx
+++ taskd_next/src/app/page.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-dfgdfg
+    <main className="w-full h-screen text-off-white">
+        <Image
+            src={'/assets/images/taskd_main_logo.svg'}
+            alt={"task main logo"}
+            width={300}
+        />
     </main>
   );
 }
