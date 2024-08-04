c1a130b56c3faafa9ae21efae7ee783eff583c6a
aaron-nerox <haroune.official2@gmail.com>
2024-08-04 20:49:44 +0400

    add: created the global button component

---
+++ taskd_next/src/app/components/Button.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function TextIconButton(
+    props: {
+        text: string,
+        icon: string,
+        onButtonClick: () => void
+    }
+) {
+    return <div
+        className="bg-off-white text-dark rounded-full inline-flex flex-row items-center justify-center px-4 py-2 gap-x-3"
+        onClick={props.onButtonClick}
+    >
+        {props.icon &&
+            <Image
+                src={props.icon}
+                alt={"button icon"}
+                height={24}
+                width={24}
+                className="h-5 w-5"
+            />
+        }
+
+        <p className="text-lg font-bold cursor-pointer select-none">
+            {props.text}
+        </p>
+    </div>
+}
